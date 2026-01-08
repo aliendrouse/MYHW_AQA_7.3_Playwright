f0aecc34c40f18209bec3bae1f0617b982ce70d4
MarinaM <jokerbunny@gmail.com>
2026-01-09 01:36:31 +0300

    tests added

--- 7.3/playwright/tests/App.spec.js
+++ 7.3/playwright/tests/App.spec.js
@@ -1,22 +1,31 @@
-const { test, expect } = require("@playwright/test");
+import { test, expect } from "@playwright/test";
+const { email, password, invalidEmail } = require("../user.js");
 
-test("test", async ({ page }) => {
-  // Go to https://netology.ru/free/management#/
-  await page.goto("https://netology.ru/free/management#/");
+test("testValid", async ({ page }) => {
+  await page.goto("https://netology.ru/");
+  await page.getByRole("link", { name: "Войти" }).click();
+  await page.goto("https://netology.ru/?modal=sign_in");
+  await page.locator(".styles_button__MYGdj").click();
+  await page.getByPlaceholder("Email").click();
+  await page.getByPlaceholder("Email").fill(email);
+  await page.getByPlaceholder("Пароль").click();
+  await page.getByPlaceholder("Пароль").fill(password);
+  await page.locator(".styles_button__MYGdj").click();
 
-  // Click a
-  await page.click("a");
-  await expect(page).toHaveURL("https://netology.ru/");
+  await page.locator('div.SmartCaptcha-Overlay_visible').count() > 0;
+  await page.locator('text=Press in the following order').count() > 0;
+});
 
-  // Click text=Учиться бесплатно
-  await page.click("text=Учиться бесплатно");
-  await expect(page).toHaveURL("https://netology.ru/free");
 
-  page.click("text=Бизнес и управление");
-
-  // Click text=Как перенести своё дело в онлайн
-  await page.click("text=Как перенести своё дело в онлайн");
-  await expect(page).toHaveURL(
-    "https://netology.ru/programs/kak-perenesti-svoyo-delo-v-onlajn-bp"
-  );
+test("testInvalid", async ({ page }) => {
+  await page.goto("https://netology.ru/");
+  await page.getByRole("link", { name: "Войти" }).click();
+  await page.locator(".styles_button__MYGdj").click();
+  await page.getByPlaceholder("Email").click();
+  await page.getByPlaceholder("Email").fill(invalidEmail);
+  await page.getByPlaceholder("Пароль").click();
+  await page.getByPlaceholder("Пароль").fill(password);
+  await page.locator(".styles_button__MYGdj").click();
+  
+  await expect(page.getByText("Неверный email")).toBeVisible();
 });
